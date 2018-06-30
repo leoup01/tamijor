dd7c5785490954bd86f7c3e3fa3dbe2e0f3df62b
leoup01 <leoup01@hotmail.com>
2018-06-29 18:04:26 -0600

    testimonials

---
+++ src/components/public/SliderTestimonials.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import '../../slider-animations.css';
+
+const content = [
+  {
+    title: 'Vulputate Mollis Ultricies Fermentum Parturient',
+    description:
+    'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.'
+  },
+  {
+    title: 'Tortor Dapibus Commodo Aenean Quam',
+    description:
+    'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.'
+  },
+  {
+    title: 'Phasellus volutpat metus',
+    description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.'
+  }
+];
+
+const SliderTestimonials = (props) => {
+  return (
+  	<section>
+  	<Slider className="slider-wrapper2" autoplay="5000">
+			{content.map((item, index) => (
+				<div
+					key={index}
+					className="slider-content"
+					//style={{ background: `url('${item.image}') no-repeat center center` }}
+				>
+					<div className="inner2">
+            <p>{item.description}</p>
+						<h3>{item.title}</h3>
+
+					</div>
+				</div>
+			))}
+	</Slider>
+	</section>
+  );
+};
+
+export default SliderTestimonials;
--- src/components/public/Testimonials.js
+++ src/components/public/Testimonials.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import { Row, Col, Container } from 'reactstrap';
+import ScrollAnimation from 'react-animate-on-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import faBeer from '@fortawesome/fontawesome-free-solid/faBeer'
 import '../../section-parallax.css';
+
+import SliderTestimonials from './SliderTestimonials';
+//import '../../testimonials.css';
+
+const Testimonials3 = (props) => {
+  return (
+        <section id="testimonial">
+            <div class="testimonial_overlay">
+            </div>
+            <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="testimonial_header text-center">
+                                <h2>testimonials</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <section id="carousel">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <div class="carousel slide" id="fade-quote-carousel" data-ride="carousel" data-interval="3000">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#fade-quote-carousel" data-slide-to="0" class="active"></li>
+                                            <li data-target="#fade-quote-carousel" data-slide-to="1"></li>
+                                        </ol>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </section>
+            </div>
+        </section>
+  );
+};
+
 
 const Testimonials = (props) => {
   return (
@@ -15,6 +56,7 @@ const Testimonials = (props) => {
                     </div>
                 </Col>
             </Row>
+            <SliderTestimonials/>
         </Container>
     </section>
   );
